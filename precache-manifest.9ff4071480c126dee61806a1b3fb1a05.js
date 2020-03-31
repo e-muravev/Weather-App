2f2ece6a7561ce9f81a977a412d1e57e559421b2
e-muravev <e.muravev.js@gmail.com>
2020-03-31 18:42:31 +0700

    Updates

---
+++ precache-manifest.9ff4071480c126dee61806a1b3fb1a05.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e170b5347cfff33ab6faf5dd43ef621",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a1c7cc0e043daaeeba09",
+    "url": "/weather-app/static/css/main.4e737633.chunk.css"
+  },
+  {
+    "revision": "878f74d564d3907611a6",
+    "url": "/weather-app/static/js/2.eedbdece.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-app/static/js/2.eedbdece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c7cc0e043daaeeba09",
+    "url": "/weather-app/static/js/main.2103674d.chunk.js"
+  },
+  {
+    "revision": "089fb1fee07ab9da1ce8",
+    "url": "/weather-app/static/js/runtime-main.029e69d5.js"
+  },
+  {
+    "revision": "b4180e59e1438082cc2951b518f00992",
+    "url": "/weather-app/static/media/humidity.b4180e59.png"
+  },
+  {
+    "revision": "7f492e2d8008bd33e9fc7bd61efed314",
+    "url": "/weather-app/static/media/logo.7f492e2d.png"
+  },
+  {
+    "revision": "06888416bc84a05c299e307736d96a3b",
+    "url": "/weather-app/static/media/pressure.06888416.png"
+  },
+  {
+    "revision": "6ca0da8a82275b1b65e2fb591efcccf9",
+    "url": "/weather-app/static/media/wind.6ca0da8a.png"
+  }
+]);
