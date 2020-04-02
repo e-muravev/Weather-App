3c7f9c7758eaa2021176beb91b5d8604167584f7
e-muravev <e.muravev.js@gmail.com>
2020-04-02 18:07:22 +0700

    fix bugs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,11 @@ import logo from './logo.png'
 
 function Header() {
 	return (
-		<header className="header">
-			<h2>Weather App</h2>
-			<img src={logo} alt="logo" style={{width: '30px', paddingBottom: '3px'}}/>
+		<header style={{width: '100%', backgroundColor: 'rgba(0,0,0,.75)'}}>
+			<div className="header">
+				<h2>Weather App</h2>
+				<img src={logo} alt="logo"/>
+			</div>
 		</header>
 	);
 }
