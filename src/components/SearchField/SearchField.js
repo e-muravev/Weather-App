2a067b9d4b78778d1dea05bd289752de01240518
e-muravev <e.muravev.js@gmail.com>
2020-05-24 22:18:54 +0700

    fix some ccs

--- src/components/SearchField/SearchField.js
+++ src/components/SearchField/SearchField.js
@@ -76,7 +76,7 @@ function SearchField({ search }) {
       				return (
       					<div key={region.id} className="geo" onClick={() => getAutocompleteData(region.name)}>
       						<img style={{marginRight: '5px'}} src={geometka} alt='geometka' width='20px' height='25px'/>
-      						<p >{region.name}</p>
+      						<p style={{cursor: 'pointer'}}>{region.name}</p>
       					</div>
       				); 
       			}) : null}
